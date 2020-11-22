ed1f7f62606c081655d84cc9c6168b9b46fd2367
Bárbara Oliveira <babyr.oliveira@gmail.com>
2020-11-22 03:09:07 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DeptManagerr.db;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeptManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cd;
+
+            Console.Write("Olá usuário, digite o código do departamento:");
+            cd = Console.ReadLine();
+
+            using (var db = new employeesContext())
+            {
+                var GerenteDoDepartamento = db.DeptManager
+                .Include(gd => gd.DeptNoNavigation) 
+                .Include(gd => gd.EmpNoNavigation)
+                .Where(gd => gd.DeptNoNavigation.DeptNo == cd)
+                .OrderBy(gd => gd.ToDate);
+
+                foreach (var GerenteDepartamento in GerenteDoDepartamento)
+                {
+                    Departments depart = GerenteDepartamento.DeptNoNavigation;
+                    Employees ger = GerenteDepartamento.EmpNoNavigation;
+
+                Console.WriteLine($"{depart.DeptName}, gerido por {ger.LastName}.");
+                }
+
+            }
+        }
+    }
+}
---
+++ db/Salaries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeptManagerr.db
+{
+    public partial class Salaries
+    {
+        public int EmpNo { get; set; }
+        public int Salary { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+
+        public virtual Employees EmpNoNavigation { get; set; }
+    }
+}
